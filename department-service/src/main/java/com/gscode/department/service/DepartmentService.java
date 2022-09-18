3ac3c7b21b9e97a1a1d8009180b7039140cce483
govindsolanketech <govind.solanke@outlook.com>
2022-09-19 01:26:27 +0530

    initial commit

---
+++ department-service/src/main/java/com/gscode/department/service/DepartmentService.java
@@ -0,0 +1,25 @@
+package com.gscode.department.service;
+
+import com.gscode.department.entity.Department;
+import com.gscode.department.repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.save(department);
+    }
+
+    public Department findDepartmentById(Long departmentId) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.findByDepartmentId(departmentId);
+    }
+}
---
+++ user-service/src/main/java/com/gscode/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gscode.user.repository;
+
+import com.gscode.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User,Long> {
+    User findByUserId(Long userId);
+}
