ec75d8295b3602721feddf529d4bfd2d342a896a
Govind Solanke <113302457+govindusolanke@users.noreply.github.com>
2022-10-07 13:02:14 +0530

    Updated packages
    Updated packages

--- cloud-config-server/src/main/java/com/gscode/cloud/CloudConfigServerApplication.java
+++ cloud-config-server/src/main/java/com/gscode/cloud/CloudConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.dailycodebuffer.cloud;
+package com.gscode.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
